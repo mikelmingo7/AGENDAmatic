5991715a0835bfc436bb99e19a6fce9dcce5f62c
Julen396 <julen396@gmail.com>
2021-04-01 22:59:53 +0200

    Menu

--- AGENDAmatic/main.c
+++ AGENDAmatic/main.c
@@ -1,10 +1,45 @@
-/*
- * main.c
- *
- *  Created on: 31 mar. 2021
- *      Author: Julen
- */
 #include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+void echo(void)
+{
+	char opcion;
+
+	do
+    {
+		printf("  \n");
+		printf("  1.Opcion \n");
+		printf("  2.Opcion \n");
+		printf("  3.Salir \n");
+
+		do
+		{
+			printf( "  Introduce una opcion : \n");
+			fflush( stdin );
+			scanf( "%c", &opcion );
+
+		} while ( opcion < '1' || opcion > '3' );
+
+		switch ( opcion )
+		{
+			case '1':
+				printf("  \n");
+				printf("Has elegido la opcion: 1 \n");
+
+				break;
+
+			case '2':
+				printf("  \n");
+				printf("Has elegido la opcion: 2 \n");
+
+				break;
+
+		}
+
+    } while ( opcion != '3' );
+
+}
 
 int main(void)
 {
@@ -12,6 +47,7 @@ int main(void)
 	printf("Bienvenido a AgendaMatic\n");
 	printf("Hola!");
 	printf("awa");
+	echo();
 	return 0;
 }
 
