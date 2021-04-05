741bad335ebf8661a69ad970723e2195365d8cca
mikel andoni mingo <mikelmingo@opendeusto.es>
2021-04-05 19:09:38 +0200

    opciones del menu introducidas

--- AGENDAmatic/main.c
+++ AGENDAmatic/main.c
@@ -5,13 +5,30 @@
  *      Author: Julen
  */
 #include <stdio.h>
+#include<stdlib.h>
 
 int main(void)
 {
-	printf("Hola soy AGENDAmatic!\n");
-	printf("Bienvenido a AgendaMatic\n");
-	printf("Hola!");
-	printf("awa");
+	int opcion;
+	do {
+		printf("1. Opcion\n");
+		printf("2. Opcion\n");
+		print("3. Opcion (Salir)");
+
+
+	switch(opcion){
+	case 1;
+	break;
+
+	case 2;
+	break;
+
+	case 3;
+	break;
+	}
+	
+
+	}while(opcion !=3);
 	return 0;
 }
 
